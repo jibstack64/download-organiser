a963dfb0824e563c25ac86803d7c61549a01a913
jibstack64 <jibbystar@proton.me>
2023-01-02 18:53:17 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# download-organiser
+
+![GitHub](https://img.shields.io/github/license/jibstack64/download-organiser)
+
+A small tool that organises your downloads by file extension; written for my beginner friend to learn from.
+
+---
+
+Run:
+`python download-organiser.py`
+
+All files in your `(D/d)ownloads` folder are put into their own folders by their extensions, making it easy to organise and catalogue your files.
---
+++ download-organiser.py
@@ -0,0 +1,55 @@
+# import required libraries
+from colorama import Fore as Colour
+import os
+
+def fatal(text: str, i: int = 1) -> None:
+    print(Colour.RED + text + Colour.RESET)
+    exit(i)
+
+def error(text: str) -> None:
+    print(Colour.LIGHTRED_EX + text + Colour.RESET)
+
+def exp(path: str) -> str:
+    return os.path.expanduser(path)
+
+# get correct download directory
+downloads = exp("~/downloads")
+if not os.path.isdir(downloads):
+    downloads = exp("~/Downloads") if os.path.isdir(exp("~/Downloads")) else ""
+    if downloads == "":
+        fatal("no download folder found.")
+os.chdir(downloads) # change current directory to downloads
+
+# get all files in downloads
+files = os.listdir(downloads)
+for x in files.copy():
+    if os.path.isdir(f"{downloads}/{x}"):
+        files.remove(x)
+mapping: dict[str, list[str]] = {}
+for f in files:
+    ext = f.split(".")[-1]
+    if mapping.get(ext) == None:
+        mapping[ext] = []
+    mapping[ext].append(f)
+
+# move files and create appropriate directories
+for extension, fs in mapping.items():
+    if not os.path.isdir(extension):
+        os.mkdir(extension)
+    for file in fs:
+        os.rename(f"{downloads}/{file}", f"{downloads}/{extension}/{file}")
+
+# correct any files that are in their wrong folders
+for dir in os.listdir(downloads):
+    for file in os.listdir(dir):
+        if not file.endswith("."+dir):
+            ext = file.split(".")[-1]
+            if "." in file:
+                if not os.path.isdir(f"{downloads}/{ext}"):
+                    os.mkdir(f"{downloads}/{ext}")
+                os.rename(f"{downloads}/{dir}/{file}", f"{downloads}/{ext}/{file}")
+                error(f"'{file}' in '{dir}'; moved to '{downloads}/{ext}'.")
+            else:
+                os.rename(f"{downloads}/{dir}/{file}", f"{downloads}/{file}")
+                error(f"'{file}' in '{dir}'; moved to '{downloads}'.")
+            
